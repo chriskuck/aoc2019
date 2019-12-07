ff4a57d61aad58b566a308f861f05339c589cfa7
Chris Kuck <fkuck@covermymeds.com>
2019-12-07 15:00:08 -0500

    part2

---
+++ aoc2019/7/b.rb
@@ -0,0 +1,24 @@
+require "./intcode"
+
+exit(1) unless !ARGV[0].nil? && File.exist?(ARGV[0])
+program = read_program(ARGV[0])
+
+def try_phases(program, phases)
+  previous_output = '0\n'
+  for i in (0..4) do
+    input = StringIO.new()
+    output = StringIO.new()
+    input.write(phases[i]+",")
+    input.write(previous_output)
+    input.rewind
+    Program.new(program, input, output).process
+    output.rewind
+    previous_output = output.read
+    puts "prev[#{i}]: #{previous_output}"
+  end
+  return previous_output
+end
+
+perms = '01234'.chars.permutation.map(&:join)
+
+puts perms.map { |phases| try_phases(program, phases.chars).to_i }.max
